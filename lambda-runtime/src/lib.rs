86e52ed4d9d14567ef02c04940c8965a65185dda
Ash Hornbeck <ash.hornbeck@gmail.com>
2022-01-27 22:09:11 -0600

    Made `types` public on v0.4.1

--- lambda-runtime/src/lib.rs
+++ lambda-runtime/src/lib.rs
@@ -25,7 +25,7 @@ mod requests;
 #[cfg(test)]
 mod simulated;
 /// Types available to a Lambda function.
-mod types;
+pub mod types;
 
 use requests::{EventCompletionRequest, EventErrorRequest, IntoRequest, NextEventRequest};
 use types::Diagnostic;
